706c04df761018a0cadad3458ba2f09792c3aa11
Jashwanth <jashwanth.aerospace@gmail.com>
2024-12-23 15:03:06 +0530

    modified server.js file

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({ 
+  origin: 'https://developers-hub-basic-freelancing-website-kfga.vercel.app', // Allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+}));
 
 // Routes
 app.use('/api/user', devuserRoutes);
